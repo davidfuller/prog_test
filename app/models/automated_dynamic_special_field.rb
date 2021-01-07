2c097ccc6c9e2a6bd6ed8f30dc13c87781d0c138
David Fuller <david@muvi2.co.uk>
2021-01-07 11:39:44 +0000

    Tidy

--- app/models/automated_dynamic_special_field.rb
+++ app/models/automated_dynamic_special_field.rb
@@ -5,7 +5,6 @@ class AutomatedDynamicSpecialField < ActiveRecord::Base
 
 def reconcile_the_change()
   if template_field_join.dynamic_special_field.dynamic_special_image_spec.logo
-  
     logo = DynamicSpecialLogo.find(the_id)
     if logo
       self.the_value = logo.filename
@@ -13,17 +12,35 @@ def reconcile_the_change()
       self.save
     end
   end
+  if template_field_join.dynamic_special_field.dynamic_special_image_spec.promo
+    promo = DynamicSpecialMedia.find(the_id)
+    if promo && promo.media_file
+      self.the_value = promo.media_file.full_filename
+      self.the_display = promo.media_file.name
+      self.save
+    end
+  end
 end
 
 def dynamic_special_logo_url
+  url = nil
   if template_field_join.dynamic_special_field.dynamic_special_image_spec.logo
     if the_id
       logo = DynamicSpecialLogo.find(the_id)
       if logo
-        logo.display_url
+        url = logo.display_url
       end
     end
   end
+  if template_field_join.dynamic_special_field.dynamic_special_image_spec.promo
+    if the_id
+      promo = DynamicSpecialMedia.find(the_id)
+      if promo && promo.media_file && promo.media_file.jpeg_exist?
+        url = promo.media_file.jpeg_url
+      end
+    end
+  end
+  url
 end
 
 
