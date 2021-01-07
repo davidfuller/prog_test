e926d62e68e70d981142ab187a615b321924ec7e
David Fuller <david@muvi2.co.uk>
2021-01-07 11:52:35 +0000

    Added dynamic image spec

--- app/models/media_folder.rb
+++ app/models/media_folder.rb
@@ -3,6 +3,7 @@ class MediaFolder < ActiveRecord::Base
   belongs_to :channel
   belongs_to :aspect
   has_many :media_files
+  has_many :dynamic_special_image_specs
   
   
   def self.find_by_name_and_channel(name, channel)
