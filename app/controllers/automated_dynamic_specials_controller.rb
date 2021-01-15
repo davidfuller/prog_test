995297eaaa2291dcdaa3324c949feb830a06e064
David Fuller <david@muvi2.co.uk>
2021-01-15 10:00:04 +0000

    Added use_media and some more variables to show

--- app/controllers/automated_dynamic_specials_controller.rb
+++ app/controllers/automated_dynamic_specials_controller.rb
@@ -15,10 +15,15 @@ class AutomatedDynamicSpecialsController < ApplicationController
   def show
     @automated_dynamic_special = AutomatedDynamicSpecial.find(params[:id])
     @logo_sets = @automated_dynamic_special.logos
+    @promo_field = @automated_dynamic_special.promo_field
+    @promo_dynamic_special_media = @automated_dynamic_special.promo_dynamic_special_media(@promo_field)
+    params[:image_type] = @promo_field.field_type_name
+    @dynamic_special_medias = DynamicSpecialMedia.search(params)
 
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @automated_dynamic_special }
+      format.js 
     end
   end
 
@@ -88,5 +93,21 @@ class AutomatedDynamicSpecialsController < ApplicationController
       format.html { redirect_to(automated_dynamic_specials_url) }
       format.xml  { head :ok }
     end
+  end
+
+  def use_media
+    field = AutomatedDynamicSpecialField.find(params[:field_id])
+    if field && params[:dynamic_special_media_id]
+      field.the_id = params[:dynamic_special_media_id]
+      if field.save
+        field.reconcile_the_change()
+        flash[:notice] = 'Media File successfully updated'
+      else
+        flash[:notice] = 'Issue with updating media file'
+      end
+    else
+      flash[:notice] = 'Issue with updating media file'
+    end
+    redirect_to automated_dynamic_special_path({:id => params[:id], :search => params[:search], :page => params[:page]})
   end
 end
