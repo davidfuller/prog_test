dc14766bdad9d6b123108faee39ef052ff7c2185
David Fuller <david@muvi2.co.uk>
2021-01-15 10:39:12 +0000

    Auto filename generator

---
+++ public/javascripts/media_files_entry.js
@@ -0,0 +1,21 @@
+//contains js for media file entry
+var myForm = document.getElementById("media_file_name");
+myForm.addEventListener("focusout", getInputValue);
+
+myForm.addEventListener('keydown', function(e) {
+  if (e.keyIdentifier == 'U+000A' || e.keyIdentifier == 'Enter' || e.keyCode == 13) {
+      if (e.target.name == 'media_file[name]'){
+        document.getElementById("media_file_filename").focus();
+        e.preventDefault();
+        return false;
+      }
+  }
+}, true);
+
+function getInputValue() {
+  let prefix = document.getElementById("media_file_next_prefix").value;
+  let inputVal = document.getElementById("media_file_name").value;
+  var filenameField = document.getElementById("media_file_filename");
+  var filename = (prefix + inputVal).toUpperCase() + ".tga";
+  filenameField.value = filename;
+}
