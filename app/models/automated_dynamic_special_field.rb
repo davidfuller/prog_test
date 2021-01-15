011a95f24c8627842899ec712005b53ea668b758
David Fuller <david@muvi2.co.uk>
2021-01-15 10:08:13 +0000

    Added a couple of helper methids

--- app/models/automated_dynamic_special_field.rb
+++ app/models/automated_dynamic_special_field.rb
@@ -22,26 +22,33 @@ def reconcile_the_change()
   end
 end
 
-def dynamic_special_logo_url
-  url = nil
-  if template_field_join.dynamic_special_field.dynamic_special_image_spec.logo
-    if the_id
-      logo = DynamicSpecialLogo.find(the_id)
-      if logo
-        url = logo.display_url
+  def dynamic_special_logo_url
+    url = nil
+    if template_field_join.dynamic_special_field.dynamic_special_image_spec.logo
+      if the_id
+        logo = DynamicSpecialLogo.find(the_id)
+        if logo
+          url = logo.display_url
+        end
       end
     end
-  end
-  if template_field_join.dynamic_special_field.dynamic_special_image_spec.promo
-    if the_id
-      promo = DynamicSpecialMedia.find(the_id)
-      if promo && promo.media_file && promo.media_file.jpeg_exist?
-        url = promo.media_file.jpeg_url
+    if template_field_join.dynamic_special_field.dynamic_special_image_spec.promo
+      if the_id
+        promo = DynamicSpecialMedia.find_by_id(the_id)
+        if promo && promo.media_file && promo.media_file.jpeg_exist?
+          url = promo.media_file.jpeg_url
+        end
       end
     end
+    url
   end
-  url
-end
 
+  def field_type_name
+    template_field_join.dynamic_special_field.dynamic_special_image_spec.name
+  end
+
+  def field_number
+    template_field_join.field
+  end
 
 end
