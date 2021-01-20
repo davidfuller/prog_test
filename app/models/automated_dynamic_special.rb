dbceafd8b4dc60e3a613d437e10f0814f49547ec
David Fuller <david@muvi2.co.uk>
2021-01-20 18:02:28 +0000

    Added search and lots more

--- app/models/automated_dynamic_special.rb
+++ app/models/automated_dynamic_special.rb
@@ -4,6 +4,93 @@ class AutomatedDynamicSpecial < ActiveRecord::Base
   belongs_to :dynamic_special_template
   has_many :automated_dynamic_special_fields, :dependent => :destroy
 
+  validates_presence_of :name
+  validates_uniqueness_of :name, :scope => :channel_id , :message => 'has been already taken for this channel'
+  validates_presence_of :channel
+  validate :no_blank_template
+
+  default_scope :order => :name
+
+  PER_PAGE = 12
+
+  def self.search(params)
+    channel_id = Channel.find_by_name(params[:channel])
+    current_time = Time.current
+    search = params[:search]
+    show_all = params[:show_all].present?
+    page = params[:page]
+    show_only = params[:show_only].present?
+
+    if channel_id # has channel
+      if search # has search (including '')
+        if show_all # including in the past
+          if show_only # just the past
+            paginate  :all, :per_page => PER_PAGE , :page => page, 
+            :conditions => ['channel_id = ? AND name LIKE ? AND last_use < ?', channel_id, "%#{search}%", current_time]
+          else
+            paginate  :all, :per_page => PER_PAGE , :page => page, 
+                      :conditions => ['channel_id = ? AND name LIKE ?', channel_id, "%#{search}%"]
+          end
+        else 
+          if show_only # just the past
+            paginate  :all, :per_page => PER_PAGE , :page => page, 
+                      :conditions => ['channel_id = ? AND name LIKE ? AND last_use < ?', channel_id, "%#{search}%", current_time]
+          else # just the future
+            paginate  :all, :per_page => PER_PAGE , :page => page, 
+                      :conditions => ['channel_id = ? AND name LIKE ? AND last_use >= ?', channel_id, "%#{search}%", current_time]
+          end
+        end
+      else # blank search
+        if show_only # just the past
+          paginate  :all, :per_page => PER_PAGE , :page => page, 
+                    :conditions => ['channel_id = ? AND last_use < ?', channel_id, current_time]
+        else
+          paginate  :all, :per_page => PER_PAGE , :page => page, 
+                    :conditions => ['channel_id = ?', channel_id]
+        end
+      end
+    else # All channels
+      if search then # has search (including '')
+        if show_all # including the past
+          if show_only # just the past
+            paginate  :all, :per_page => PER_PAGE , :page => page, 
+            :conditions => ['name LIKE ? AND last_use < ?', "%#{search}%", current_time]
+          else
+            paginate  :all, :per_page => PER_PAGE , :page => page, 
+            :conditions => ['name LIKE ?', "%#{search}%"]
+          end
+        else
+          if show_only # just the past
+            paginate  :all, :per_page => PER_PAGE , :page => page, 
+                      :conditions => ['name LIKE ? AND last_use < ?', "%#{search}%", current_time]
+          else # just the future
+            paginate  :all, :per_page => PER_PAGE , :page => page, 
+                      :conditions => ['name LIKE ? AND last_use >= ?', "%#{search}%", current_time]
+          end
+        end
+      else # empty search
+        if show_all #including the past
+          if show_only # just the past
+            paginate  :all, :per_page => PER_PAGE, :page => page, 
+                      :conditions => ['last_use < ?', current_time]
+          else
+            paginate  :all, :per_page => PER_PAGE , :page => page 
+          end
+        else
+          if show_only # just the past
+            paginate  :all, :per_page => PER_PAGE, :page => page, 
+                      :conditions => ['last_use < ?', current_time]
+          else
+            paginate  :all, :per_page => PER_PAGE, :page => page, 
+                      :conditions => ['last_use >= ?', current_time]
+          end
+        end
+      end
+    end
+  end
+
+
+
   def channel_name
     if channel
       channel.name
@@ -95,4 +182,115 @@ class AutomatedDynamicSpecial < ActiveRecord::Base
     end
   end
 
+  def no_blank_template
+    errors.add_to_base('You must select a template') if dynamic_special_template_id.blank?
+  end
+
+  def add_special_fields(original)
+    original.automated_dynamic_special_fields.each do |field|
+      new_field = AutomatedDynamicSpecialField.find(field).clone
+      new_field.automated_dynamic_special_id = self.id
+      new_field.save
+    end
+  end
+
+  def field_list
+    fields = automated_dynamic_special_fields
+    results = []
+    fields.each do |field|
+      result = {}
+      result[:value] = field.the_value
+      result[:id] = field.the_id
+      template = field.template_field_join
+      if template
+        result[:field_number] = template.field
+        result[:fixed] = template.fixed
+        if template.dynamic_special_field
+          spec = template.dynamic_special_field.dynamic_special_image_spec
+          if spec
+            if spec.name == 'Text'
+              result[:type] = :text
+            elsif spec.logo
+              result[:type] = :logo
+            elsif spec.promo
+              result[:type] = :promo
+              media = promo_dynamic_special_media(field)
+              if media && media.media_file && media.media_file.status
+                result[:media_ready] = media.media_file.status.message
+              else
+                result[:media_ready] = 'No media'
+              end
+            else
+              result[:type] = :nothing
+            end
+          end
+        end
+      end
+      results << result
+    end
+    results
+  end
+
+  def self.all_text_fields_empty(test_field_list)
+    test_field_list.each do |field|
+      if field[:type] == :text && !field[:fixed] && field[:value].present?
+        return false
+      end
+    end
+    true
+  end
+
+  def self.any_missing_logo(test_field_list)
+    test_field_list.each do |field|
+      if field[:type] == :logo && !field[:fixed] && field[:value].blank?
+        return true
+      end
+    end
+    false
+  end
+
+  def self.any_missing_promos(test_field_list)
+    test_field_list.each do |field|
+      if field[:type] == :promo && !field[:fixed] && field[:value].blank?
+        return true
+      end
+    end
+    false
+  end
+
+  def self.any_media_not_ready(test_field_list)
+    test_field_list.each do |field|
+      if field[:type] == :promo && !field[:fixed] && field[:media_ready] != 'Ready'
+        return true
+      end
+    end
+    false
+  end
+
+  def short_ready_message
+    list = field_list
+    message = ''
+    details =[]
+    if self.class.all_text_fields_empty(list)
+      if message.blank? then message = 'Text fields empty' else message = 'Multiple issues' end
+      details << 'Text fields empty'  
+    end
+    if self.class.any_missing_logo(list)
+      if message.blank? then message = 'Missing logo' else message = 'Multiple issues' end
+      details << 'Missing logo'  
+    end
+    if self.class.any_missing_promos(list)
+      if message.blank? then message = 'Missing promo' else message = 'Multiple issues' end
+      details << 'Missing promo'
+    end
+    if self.class.any_media_not_ready(list)
+      if message.blank? then message = 'Media not ready' else message = 'Multiple issues' end
+      details << 'Media not ready'
+    end
+    if message.blank? 
+      message = 'Good'
+      details << 'Good'
+    end
+    {:message => message, :details => details}
+  end
 end
