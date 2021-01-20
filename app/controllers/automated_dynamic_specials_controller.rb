9b1fa4fcd1a2ab133cd9b240d6e377288b7c7e57
David Fuller <david@muvi2.co.uk>
2021-01-20 17:59:40 +0000

    Added search, index_params and multiple methods

--- app/controllers/automated_dynamic_specials_controller.rb
+++ app/controllers/automated_dynamic_specials_controller.rb
@@ -2,10 +2,13 @@ class AutomatedDynamicSpecialsController < ApplicationController
   # GET /automated_dynamic_specials
   # GET /automated_dynamic_specials.xml
   def index
-    @automated_dynamic_specials = AutomatedDynamicSpecial.all
-
+    @automated_dynamic_specials = AutomatedDynamicSpecial.search(params)
+    
     respond_to do |format|
-      format.html # index.html.erb
+      format.html do
+        @channel_display = Channel.display
+        @show_duplicates = params[:show_duplicates]
+      end# index.html.erb
       format.xml  { render :xml => @automated_dynamic_specials }
     end
   end
@@ -17,9 +20,11 @@ class AutomatedDynamicSpecialsController < ApplicationController
     @logo_sets = @automated_dynamic_special.logos
     @promo_field = @automated_dynamic_special.promo_field
     @promo_dynamic_special_media = @automated_dynamic_special.promo_dynamic_special_media(@promo_field)
-    params[:image_type] = @promo_field.field_type_name
+    if @promo_field
+      params[:image_type] = @promo_field.field_type_name
+    end
     @dynamic_special_medias = DynamicSpecialMedia.search(params)
-
+    @index_params = clean_params(params[:index_params])
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @automated_dynamic_special }
@@ -34,6 +39,7 @@ class AutomatedDynamicSpecialsController < ApplicationController
     @automated_dynamic_special.fix_nil_last_use
     @channels = Channel.all
     @templates = DynamicSpecialTemplate.all
+    @index_params = clean_params(params[:index_params])
 
     respond_to do |format|
       format.html # new.html.erb
@@ -46,21 +52,35 @@ class AutomatedDynamicSpecialsController < ApplicationController
     @channels = Channel.all
     @templates = DynamicSpecialTemplate.all
     @automated_dynamic_special = AutomatedDynamicSpecial.find(params[:id])
+    @index_params = clean_params(params[:index_params])
   end
 
   # POST /automated_dynamic_specials
   # POST /automated_dynamic_specials.xml
   def create
     @automated_dynamic_special = AutomatedDynamicSpecial.new(params[:automated_dynamic_special])
-    @automated_dynamic_special.create_fields
-
+    @index_params = clean_params(params[:index_params])
     respond_to do |format|
       if @automated_dynamic_special.save
-        flash[:notice] = 'Automated Dynamic Special was successfully created.'
-        format.html { redirect_to(@automated_dynamic_special) }
-        format.xml  { render :xml => @automated_dynamic_special, :status => :created, :location => @automated_dynamic_special }
+        @automated_dynamic_special.create_fields
+        if @automated_dynamic_special.save
+          flash[:notice] = 'Automated Dynamic Special was successfully created.'
+          format.html {redirect_to automated_dynamic_special_path(@automated_dynamic_special, :index_params => @index_params) }
+          format.xml  { render :xml => @automated_dynamic_special, :status => :created, :location => @automated_dynamic_special }
+        else
+          format.html do
+            @channels = Channel.all
+            @templates = DynamicSpecialTemplate.all
+            render :action => "new" 
+          end 
+          format.xml  { render :xml => @automated_dynamic_special.errors, :status => :unprocessable_entity }
+        end
       else
-        format.html { render :action => "new" }
+        format.html do
+          @channels = Channel.all
+          @templates = DynamicSpecialTemplate.all
+          render :action => "new"
+        end 
         format.xml  { render :xml => @automated_dynamic_special.errors, :status => :unprocessable_entity }
       end
     end
@@ -70,14 +90,19 @@ class AutomatedDynamicSpecialsController < ApplicationController
   # PUT /automated_dynamic_specials/1.xml
   def update
     @automated_dynamic_special = AutomatedDynamicSpecial.find(params[:id])
+    @index_params = clean_params(params[:index_params])
 
     respond_to do |format|
       if @automated_dynamic_special.update_attributes(params[:automated_dynamic_special])
-        flash[:notice] = 'AutomatedDynamicSpecial was successfully updated.'
-        format.html { redirect_to(@automated_dynamic_special) }
+        flash[:notice] = 'Automated Dynamic Special was successfully updated.'
+        format.html { redirect_to automated_dynamic_special_path(@automated_dynamic_special, :index_params => @index_params) }
         format.xml  { head :ok }
       else
-        format.html { render :action => "edit" }
+        format.html do 
+          @channels = Channel.all
+          @templates = DynamicSpecialTemplate.all
+          render :action => "edit" 
+        end
         format.xml  { render :xml => @automated_dynamic_special.errors, :status => :unprocessable_entity }
       end
     end
@@ -88,15 +113,17 @@ class AutomatedDynamicSpecialsController < ApplicationController
   def destroy
     @automated_dynamic_special = AutomatedDynamicSpecial.find(params[:id])
     @automated_dynamic_special.destroy
+    @index_params = clean_params(params[:index_params])
 
     respond_to do |format|
-      format.html { redirect_to(automated_dynamic_specials_url) }
+      format.html { redirect_to automated_dynamic_specials_url(@index_params)}
       format.xml  { head :ok }
     end
   end
 
   def use_media
     field = AutomatedDynamicSpecialField.find(params[:field_id])
+    @index_params = clean_params(params[:index_params])
     if field && params[:dynamic_special_media_id]
       field.the_id = params[:dynamic_special_media_id]
       if field.save
@@ -108,6 +135,39 @@ class AutomatedDynamicSpecialsController < ApplicationController
     else
       flash[:notice] = 'Issue with updating media file'
     end
-    redirect_to automated_dynamic_special_path({:id => params[:id], :search => params[:search], :page => params[:page]})
+    redirect_to automated_dynamic_special_path({:id => params[:id], :search => params[:search], :page => params[:page], :index_params => @index_params})
   end
+
+  def duplicate
+    original = AutomatedDynamicSpecial.find(params[:id])
+    @automated_dynamic_special = original.clone
+    @automated_dynamic_special.name = original.name + ' Copy'
+    @automated_dynamic_special.save
+    @automated_dynamic_special.add_special_fields(original)
+    @channels = Channel.all
+    @templates = DynamicSpecialTemplate.all
+    @index_params = clean_params(params[:index_params])
+    flash[:notice] = "Duplicate Special created. You may want to change any details, then click Update"
+  
+    respond_to do |format|
+      format.html { render :action => 'edit'}
+      format.xml  { render :xml => @automated_dynamic_special }
+    end
+  end
+
+  def demo_image
+    @image_template = DynamicSpecialTemplate.find_by_id(params[:changed_template_id])
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def clean_params(params)
+    # Delete any blank params
+    if params 
+      params.delete_if {|k,v| v.blank?}
+    end
+  end
+
 end
+
