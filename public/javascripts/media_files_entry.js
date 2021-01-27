74459a5ef81e2a3366108eff1f6b95c7419393b1
David Fuller <david@muvi2.co.uk>
2021-01-27 09:58:20 +0000

    Improved filename sanitation

--- public/javascripts/media_files_entry.js
+++ public/javascripts/media_files_entry.js
@@ -16,7 +16,31 @@ function getInputValue() {
   let prefix = document.getElementById("media_file_next_prefix").value;
   let inputVal = document.getElementById("media_file_name").value;
   var filenameField = document.getElementById("media_file_filename");
-  var filename = (prefix + inputVal).toUpperCase() + ".tga";
-  var filename_normalised = filename.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace("Æ","AE");
+  var filename = (prefix + inputVal).toUpperCase();
+  var filename_normalised = sanitize(filename) + ".tga";
   filenameField.value = filename_normalised;
+}
+
+var illegalRe = /[\/\?<>\\:\*\|"]/g;
+var controlRe = /[\x00-\x1f\x80-\x9f]/g;
+var reservedRe = /^\.+$/;
+var windowsReservedRe = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/i;
+var windowsTrailingRe = /[\. ]+$/;
+
+function sanitize(input) {
+  replacement = "_"
+  var sanitized = input
+    .replace("Ø", "O")
+    .replace("Æ","AE")
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(illegalRe, replacement)
+    .replace(controlRe, replacement)
+    .replace(reservedRe, replacement)
+    .replace(windowsReservedRe, replacement)
+    .replace(windowsTrailingRe, replacement)
+    .replace(/[^a-z0-9]/gi, replacement)
+    .replace(/\s+/g, replacement)
+    .replace(/_+/g,replacement);
+  return sanitized;
 }
