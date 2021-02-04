fcf2e70ca310c04ac47bb1c3c9d8b13514c55ae4
David Fuller <David@muvi2.co.uk>
2021-02-04 15:44:53 +0000

    added media folder stuff

--- app/models/dynamic_special_logo.rb
+++ app/models/dynamic_special_logo.rb
@@ -15,4 +15,30 @@ class DynamicSpecialLogo < ActiveRecord::Base
     end
   end
 
+  def media_folder
+    if dynamic_special_image_spec && dynamic_special_image_spec.media_folder
+      dynamic_special_image_spec.media_folder
+    else
+      nil
+    end
+  end
+
+  def media_folder_name
+    folder = media_folder
+    if media_folder
+      media_folder.name
+    else
+      ""
+    end
+  end
+
+  def full_filename
+    folder = media_folder
+    if folder
+      folder.folder + filename
+    else
+      filename
+    end
+  end
+
 end
