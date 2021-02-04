4295c259bed9190adba297dc692ac764ed3d7721
David Fuller <David@muvi2.co.uk>
2021-02-04 15:42:28 +0000

    Added field calculations

--- app/models/automated_dynamic_special_field.rb
+++ app/models/automated_dynamic_special_field.rb
@@ -3,24 +3,24 @@ class AutomatedDynamicSpecialField < ActiveRecord::Base
   belongs_to :automated_dynamic_special
   belongs_to :template_field_join
 
-def reconcile_the_change()
-  if template_field_join.dynamic_special_field.dynamic_special_image_spec.logo
-    logo = DynamicSpecialLogo.find(the_id)
-    if logo
-      self.the_value = logo.filename
-      self.the_display = logo.name
-      self.save
+  def reconcile_the_change()
+    if template_field_join.dynamic_special_field.dynamic_special_image_spec.logo
+      logo = DynamicSpecialLogo.find(the_id)
+      if logo
+        self.the_value = logo.filename
+        self.the_display = logo.name
+        self.save
+      end
+    end
+    if template_field_join.dynamic_special_field.dynamic_special_image_spec.promo
+      promo = DynamicSpecialMedia.find(the_id)
+      if promo && promo.media_file
+        self.the_value = promo.media_file.full_filename
+        self.the_display = promo.media_file.name
+        self.save
+      end
     end
   end
-  if template_field_join.dynamic_special_field.dynamic_special_image_spec.promo
-    promo = DynamicSpecialMedia.find(the_id)
-    if promo && promo.media_file
-      self.the_value = promo.media_file.full_filename
-      self.the_display = promo.media_file.name
-      self.save
-    end
-  end
-end
 
   def dynamic_special_logo_url
     url = nil
@@ -44,11 +44,72 @@ end
   end
 
   def field_type_name
-    template_field_join.dynamic_special_field.dynamic_special_image_spec.name
+    if template_field_join && template_field_join.dynamic_special_field && template_field_join.dynamic_special_field.dynamic_special_image_spec
+      template_field_join.dynamic_special_field.dynamic_special_image_spec.name
+    else
+      ""
+    end
   end
 
   def field_number
-    template_field_join.field
+    if template_field_join
+      template_field_join.field
+    else
+      ""
+    end
+  end
+
+  def field_name
+    if template_field_join && template_field_join.dynamic_special_field
+      template_field_join.dynamic_special_field.name
+    end
+  end
+
+  def field_value
+    if template_field_join && template_field_join.dynamic_special_field && template_field_join.dynamic_special_field.dynamic_special_image_spec
+      spec = template_field_join.dynamic_special_field.dynamic_special_image_spec
+      if spec
+        if spec.name == 'Text'
+          return the_value
+        elsif spec.name == 'Switcher'
+          return the_value
+        elsif spec.logo
+          logo_full_filename
+        elsif spec.promo
+          promo_full_filename
+        end
+      else
+        ""
+      end
+    else
+      ""
+    end
+  end
+
+  def promo_full_filename
+    filename = nil
+    if template_field_join.dynamic_special_field.dynamic_special_image_spec.promo
+      if the_id
+        promo = DynamicSpecialMedia.find_by_id(the_id)
+        if promo && promo.media_file
+          filename = promo.media_file.full_filename
+        end
+      end
+    end
+    filename
+  end
+
+  def logo_full_filename
+    filename = nil
+    if template_field_join.dynamic_special_field.dynamic_special_image_spec.logo
+      if the_id
+        logo = DynamicSpecialLogo.find(the_id)
+        if logo
+          filename = logo.full_filename
+        end
+      end
+    end
+    filename
   end
 
 end
