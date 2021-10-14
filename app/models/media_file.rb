5f077171bc2b9a312749133b320055e2f78672a0
David Fuller <david@muvi2.co.uk>
2021-10-14 11:55:08 +0100

    Lots of changes for resize + crop

--- app/models/media_file.rb
+++ app/models/media_file.rb
@@ -11,13 +11,14 @@ class MediaFile < ActiveRecord::Base
   
   default_scope :order => 'first_use, media_type_id, filename'
   validates_uniqueness_of :filename, :message => " is already in the system"
-  validates_uniqueness_of :name, :scope => :media_type_id, :message => " is already in the system for this media type"
+  validates_uniqueness_of :name, :if => :not_portrait_media?, :message => " is already in the system for this media type"
 
   MAX_FOLDER_CLIPS = 450
   MAX_FOLDER_WITH_AUDIO_CLIPS = 225
   
   require 'useful'
-  
+  require 'shellwords'
+
   attr_accessor :source
   attr_accessor :promo_id
   attr_accessor :notice
@@ -28,6 +29,7 @@ class MediaFile < ActiveRecord::Base
   attr_accessor :next_prefix
   attr_accessor :automated_dynamic_special_id
   attr_accessor :field_id
+  attr_accessor :resize
   
   def self.search(params)
     if params[:filename]
@@ -183,6 +185,10 @@ class MediaFile < ActiveRecord::Base
   def targa_filename
     File.basename(filename, ".*") + ".tga"
   end
+
+  def png_filename
+    File.basename(filename, ".*") + ".png"
+  end
   
   def jpeg_filename
     File.basename(filename, ".*") + ".jpg"
@@ -190,6 +196,14 @@ class MediaFile < ActiveRecord::Base
   
   def ppv_filename
     File.basename(filename, ".*") + ".ppv"
+  end
+
+  def temp_folder
+    "#{Rails.root}/public/temp/"
+  end
+
+  def temp_png
+    '/temp/'+ png_filename
   end
   
 
@@ -335,6 +349,7 @@ class MediaFile < ActiveRecord::Base
     if media
       upload_file = params[:file]
       media.issue = false
+      media.resize = false
       case media.media_type.name
       when 'Promo Clip'
         case upload_file.content_type 
@@ -442,10 +457,12 @@ class MediaFile < ActiveRecord::Base
           do_it = false
         end
       when 'Special Media'
+        #sort out temporary filename of temporary file
+        #different for tga, png and jpeg
         case upload_file.content_type 
-        when 'image/targa', 'image/x-targa', 'application/octet-stream'
+        when 'image/targa', 'image/x-targa', 'application/octet-stream', 'image/png', 'image/jpeg'
           if media.dynamic_special_media && media.dynamic_special_media.dynamic_special_image_spec
-            filename = Pathname.new("#{Rails.root}/public" + media.dynamic_special_media.dynamic_special_image_spec.upload_folder + media.targa_filename)
+            filename = media.filename_for_content_type(upload_file.content_type)
             do_it = true
           else
             media.notice = "Issue with media type: " 
@@ -471,16 +488,36 @@ class MediaFile < ActiveRecord::Base
         end
         media.original_filename = upload_file.original_filename
         media.notice = upload_file.original_filename + " uploaded. Content type: " + upload_file.content_type
-        media.status = Status.find_by_message('Queued')
-        case media.media_type.name
-        when 'Promo Clip'
-          case upload_file.content_type 
-          when 'application/octet-stream'
-            wd_copy(media.original_filename, filename)
+        if media.media_type.name == 'Special Media'
+          media.automated_dynamic_special_id = params[:automated_dynamic_special_id]
+          #process the file
+          result = media.process_special_media_upload(filename, upload_file.content_type)
+          # returns a hash of
+            # :action => one of :use, :resize, :reject
+            # :reason => text of a message as to why rejected
+            # :size => hash of width x height
+          if result[:action] == :use
+            # copy to the upload folder
+            media.copy_special_to_upload_folder
+          elsif result[:action] == :resize
+            media.resize = true
+
+          else
+            media.issue =true
+            media.notice = result[:reason]
           end
-        when 'Special Preview'
-          media.first_use = Time.current
-          media.last_use = Time.current + 3.months
+        else
+          media.status = Status.find_by_message('Queued')
+          case media.media_type.name
+          when 'Promo Clip'
+            case upload_file.content_type 
+            when 'application/octet-stream'
+              wd_copy(media.original_filename, filename)
+            end
+          when 'Special Preview'
+            media.first_use = Time.current
+            media.last_use = Time.current + 3.months
+          end
         end
       end
     end
@@ -567,6 +604,10 @@ def self.wd_copy(original_filename, filename)
 
   def clip_special_jpeg_proxy_folder
     Rails.root.join('public','data', 'proxies', 'special', 'jpeg')
+  end
+
+  def specials_original_path
+    Rails.root.join('public','data', 'special', 'Originals')
   end
 
   def dynamic_special_proxy_folder
@@ -771,12 +812,41 @@ def self.wd_copy(original_filename, filename)
     size
   end
 
+  def media_type_resizeable
+    result = false
+    if media_type
+      if media_type.name == 'Special Media'
+        if dynamic_special_media && dynamic_special_media.dynamic_special_image_spec
+            result = dynamic_special_media.dynamic_special_image_spec.resizable
+        end
+      end
+    end
+    result
+  end
+
+  def media_dimensions
+    result = {}
+    result[:valid] = false
+    result[:width] = 0
+    result[:height] = 0
+    if media_type
+      if media_type.name == 'Special Media'
+        if dynamic_special_media && dynamic_special_media.dynamic_special_image_spec
+          result[:valid] = true
+            result[:width] = dynamic_special_media.dynamic_special_image_spec.width
+            result[:height] = dynamic_special_media.dynamic_special_image_spec.height
+        end
+      end
+    end
+    result
+  end
+
   def preview_size
     size = nil
     if media_type
       if media_type.name == 'Special Media'
         if dynamic_special_media && dynamic_special_media.dynamic_special_image_spec
-            size = (dynamic_special_media.dynamic_special_image_spec.width/4).to_s + 'x' + (dynamic_special_media.dynamic_special_image_spec.height/4).to_s
+            size = (dynamic_special_media.dynamic_special_image_spec.width/2).to_s + 'x' + (dynamic_special_media.dynamic_special_image_spec.height/2).to_s
         end
       end
     end
@@ -807,7 +877,361 @@ def self.wd_copy(original_filename, filename)
       end
     end
     m
-  end    
+  end
   
-    
+  def process_special_media_upload(file, content_type)
+    # returns a hash of
+    # :action => one of :use, :resize, :reject
+    # :reason => text of a message as to why rejected
+    # :size => hash of width x height
+    case content_type 
+    when 'image/targa', 'image/x-targa', 'application/octet-stream'
+      #already a targa so check size.
+      check_size = size_match(file)
+      if check_size[:action] == :exact
+        result = {:action => :use}
+        #if size is OK then no need to resize so pass on.
+        copy_to_originals_folder(file)
+      elsif (check_size[:action] == :resize) && media_type_resizeable
+        if convert_to_png(file)
+          result = {:action => :resize, :size => check_size[:size]}
+        else
+          result = {:action => :reject, :reason => "Failed to create png file"}
+        end
+        move_to_originals_folder(file)
+      else
+        move_to_originals_folder(file)
+        result = {:action => :reject, :reason => check_size[:reason], :size => check_size[:size]}
+      end
+    when 'image/png'
+      check_size = size_match(file)
+      if check_size[:action] == :exact
+        #if size is OK then convert to targa and pass on
+        if convert_to_tga(file)
+          result = {:action => :use}
+        else
+          result = {:action => :reject, :reason => "Failed to create tga file from png"}
+        end
+        move_to_originals_folder(file)
+      elsif check_size[:action] == :resize
+        #if size is within range then send to resize.
+        result = {:action => :resize, :size => check_size[:size]}
+        copy_to_originals_folder(file)
+      else
+        #otherwise reject
+        result = {:action => :reject, :reason => check_size[:reason], :size => check_size[:size]}
+        move_to_originals_folder(file)
+      end
+    when'image/jpeg'
+      #if size is OK then convert to targa and pass on
+      check_size = size_match(file)
+      if check_size[:action] == :exact
+        #if size is OK then convert to targa and pass on
+        if convert_to_tga(file)
+          result = {:action => :use}
+        else
+          result = {:action => :reject, :reason => "Failed to create tga file from png"}
+        end
+        move_to_originals_folder(file)
+      #if size is within range then create png and send to resize.
+      elsif check_size[:action] == :resize
+        if convert_to_png(file)
+          result = {:action => :resize, :size => check_size[:size]}
+        else
+          result = {:action => :reject, :reason => "Failed to create png file"}
+        end
+        move_to_originals_folder(file)
+      else
+        #otherwise reject
+        result = {:action => :reject, :reason => check_size[:reason], :size => check_size[:size]}
+        move_to_originals_folder(file)
+      end
+    else
+      result = {:action => :reject, :reason => "Invalid content type."}
+    end
+    result
+  end
+
+  def self.image_dimensions(file)
+    result = {:valid => false}
+    if file.exist?
+      escaped_filename = file.to_s.shellescape
+      dimensions = IO.popen("identify -format %w,%h " + escaped_filename).readlines[0].strip.split(",")
+      if dimensions.length == 2
+        result = {:width => dimensions[0].to_i, :height => dimensions[1].to_i, :valid => true}
+      end
+    end
+    result
+  end
+
+  def size_match(file)
+    result = {:action => :reject}
+    dimensions = self.class.image_dimensions(file)
+    if dimensions[:valid]
+      if dynamic_special_media && dynamic_special_media.dynamic_special_image_spec
+        width = dynamic_special_media.dynamic_special_image_spec.width
+        height = dynamic_special_media.dynamic_special_image_spec.height
+        lower_limit_width = 0.8 * width
+        lower_limit_height = 0.8 * height
+        higher_limit_width = 2000
+        higher_limit_height = 2000
+
+        if width == dimensions[:width] && height == dimensions[:height]
+          result = {:action => :exact, :size => {:width => width, :height => height }}
+        else
+          if media_type_resizeable
+            if (dimensions[:width] > lower_limit_width) and (dimensions[:height] > lower_limit_height)
+              if (dimensions[:width] < higher_limit_width) and (dimensions[:height] < higher_limit_height)
+                result = {:action => :resize, :size => {:width => width, :height => height }}
+              else
+                result = {:action => :reject, :reason => 'Image too large. It must be less than ' + higher_limit_width.to_s + ' x ' + higher_limit_height.to_s + '. Uploaded size: ' + dimensions[:width].to_i.to_s + " x " + dimensions[:height].to_i.to_s, :size => {:width => width, :height => height }}
+              end
+            else
+              result = {:action => :reject, :reason => 'Image too small. It must be greater than ' + lower_limit_width.to_i.to_s + " x " + lower_limit_height.to_i.to_s + '. Uploaded size: ' + dimensions[:width].to_i.to_s + " x " + dimensions[:height].to_i.to_s, :size => {:width => dimensions[:width], :height => dimensions[:height]}}
+            end
+          else
+            result = {:action => :reject, :reason => 'Image is the wrong size. It must be ' + width.to_i.to_s + " x " + height.to_i.to_s + '. Uploaded size: ' + dimensions[:width].to_i.to_s + " x " + dimensions[:height].to_i.to_s, :size => {:width => dimensions[:width], :height => dimensions[:height]}}
+          end
+        end
+      else
+        result = {:action => :reject, :reason => "Invalid media"}
+      end
+    else
+      result = {:action => :reject, :reason => "Cannot measure dimensions"}
+    end
+    result
+  end
+
+  def convert_to_png(file)
+    if file.exist?
+      escaped_filename = file.to_s.shellescape
+      png_file = temp_folder + png_filename
+      escaped_png_filename = png_file.shellescape
+      result = IO.popen("convert " + escaped_filename + ' ' + escaped_png_filename)
+      wait_until_stable_file_size png_file
+      if result
+        true
+      else
+        false
+      end
+    else
+      false
+    end  
+  end
+
+  def wait_until_stable_file_size(filename)
+    logger.debug 'Im here=============================='
+    last_size = -1
+    for i in 1..100
+      if File.exist?(filename)
+        this_size = File.size(filename)
+        logger.debug this_size
+        if this_size ==  last_size
+          break
+        else
+          last_size = this_size
+        end
+      else
+        logger.debug 'Does not exist'
+      end
+      sleep 0.1
+    end
+  end
+
+  def convert_to_tga(file)
+    if file.exist?
+      escaped_filename = file.to_s.shellescape
+      tga_filename = temp_folder + targa_filename
+      escaped_tga_filename = tga_filename.shellescape
+      result = IO.popen("convert " + escaped_filename + ' ' + escaped_tga_filename)
+      wait_until_stable_file_size tga_filename
+      if result
+        true
+      else
+        false
+      end
+    else
+      false
+    end  
+  end
+
+  def do_resize(file, the_scale)
+    if file.exist?
+      escaped_filename = file.to_s.shellescape
+      tga_filename = temp_folder + targa_filename
+      escaped_tga_filename = tga_filename.shellescape
+      result = IO.popen("convert " + escaped_filename + ' -resize ' + (the_scale * 100).to_s + '% ' + escaped_tga_filename)
+      wait_until_stable_file_size tga_filename
+      if result
+        true
+      else
+        false
+      end
+    else
+      false
+    end 
+  end
+
+  def resize_then_crop(file, the_scale, crop_x, crop_y, offset_x, offset_y)
+    if file.exist?
+      escaped_filename = file.to_s.shellescape
+      tga_filename = temp_folder + targa_filename
+      escaped_tga_filename = tga_filename.shellescape
+      result = IO.popen("convert " + escaped_filename + ' -resize ' + (the_scale * 100).to_s + '% ' + ' -crop ' + crop_x.to_s + 'x' + crop_y.to_s + '+' + offset_x.to_s + '+' + offset_y.to_s + ' ' + escaped_tga_filename)
+      wait_until_stable_file_size tga_filename
+      if result
+        true
+      else
+        false
+      end
+    else
+      false
+    end
+  end
+
+
+  def do_the_crop(params)
+    the_x_offset = params[:x_offset].to_f.round
+    the_y_offset = params[:y_offset].to_f.round
+    the_scale = params[:scale].to_f
+
+    png_file = Pathname.new(temp_folder + png_filename)
+    dimensions = self.class.image_dimensions(png_file)
+    if dimensions[:valid]
+      if dynamic_special_media && dynamic_special_media.dynamic_special_image_spec
+        width = dynamic_special_media.dynamic_special_image_spec.width
+        height = dynamic_special_media.dynamic_special_image_spec.height
+        min_scale = width.to_f/dimensions[:width].to_f
+        if the_scale < min_scale 
+          the_scale = min_scale
+        end
+        if (the_x_offset == 0) && (the_y_offset == 0)
+          #just the scaling to do
+          if do_resize(png_file, the_scale)
+            true
+          else
+            false
+          end
+        else
+          #scale then crop
+          resize_then_crop(png_file, the_scale, width, height, the_x_offset, the_y_offset)
+        end
+      end
+    end
+    if File.exist?(png_file)
+      File.delete(png_file)
+    end
+  end
+
+  def copy_special_to_upload_folder
+    temp_targa = temp_folder + targa_filename
+    final_targa = "#{Rails.root}/public" + dynamic_special_media.dynamic_special_image_spec.upload_folder + targa_filename
+    if File.exist?(temp_targa)
+      FileUtils.copy(temp_targa, final_targa)
+      self.status = Status.find_by_message('Queued')
+      File.delete(temp_targa)
+      self.notice = targa_filename + ' uploaded.'
+    else
+      self.issue = true
+      self.notice = 'Could not copy targa image to correct folder'
+    end
+  end
+
+  def complete_the_upload
+    copy_special_to_upload_folder
+    self.save
+  end
+
+  def move_to_originals_folder(file)
+    if file.exist?
+      FileUtils.mv file, specials_original_path + original_filename
+    end
+  end
+
+  def copy_to_originals_folder(file)
+    if file.exist?
+      FileUtils.cp file, specials_original_path + original_filename
+    end
+  end
+
+  def redo_the_resize
+    #Find the original
+    original = specials_original_path + original_filename
+    if original.exist?
+      the_content_type = content_type_for_extension(original)
+      temp_filename = filename_for_content_type(the_content_type)
+      FileUtils.copy original, temp_filename
+      wait_until_stable_file_size temp_filename
+      if temp_filename.exist?
+        if the_content_type != 'image/png'
+          convert_to_png(temp_filename)
+          temp_filename.delete
+        end
+        true
+      else
+        false
+      end
+    else
+      false
+    end
+  end
+
+  def filename_for_content_type(content_type)
+    case content_type 
+    when 'image/targa', 'image/x-targa', 'application/octet-stream'
+      filename = Pathname.new(temp_folder + targa_filename)
+    when 'image/png'
+      filename = Pathname.new(temp_folder + png_filename)
+    when'image/jpeg'
+      filename = Pathname.new(temp_folder + jpeg_filename)
+    end
+    filename
+  end
+
+  def content_type_for_extension(filename)
+    case filename.extname
+    when '.jpg', 'jpeg'
+      'image/jpeg'
+    when '.png'
+      'image/png'
+    when '.tga'
+      'image/targa'
+    end
+  end
+
+  def delete_temp_png
+    filename = Pathname.new(temp_folder + png_filename)
+    if filename.exist?
+      filename.delete
+    end
+  end
+
+  def self.recent_special_previews(params)
+    type = MediaType.find_by_name('Special Preview')
+    if params[:not_loaded]
+      status = Status.find_by_message("Not loaded")
+      position = params[:pos].to_i
+      if position.nil? || position <= 0
+        position = 1
+      end
+      paginate  :per_page => 1, :page => position,
+                :conditions => ['media_type_id = ? AND status_id = ?', type.id, status.id], :order => 'created_at DESC'
+    else
+      paginate  :per_page => 5, :page =>params[:page],
+                :conditions => ['media_type_id = ?', type.id], :order => 'created_at DESC'
+    end
+  end 
+
+  def automated_dynamic_special
+    if special_preview && special_preview.automated_dynamic_special
+      special_preview.automated_dynamic_special
+    else
+      nil
+    end
+  end
+
+  def not_portrait_media?
+    portrait = MediaType.find_by_name('Portrait Still')
+    media_type_id != portrait.id
+  end
 end
