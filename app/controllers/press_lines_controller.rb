e2ed9f00e541144914544313f08d8f696fe32c07
David Fuller <david@muvi2.co.uk>
2022-10-03 18:26:09 +0100

    remove v4 and special schedule

--- app/controllers/press_lines_controller.rb
+++ app/controllers/press_lines_controller.rb
@@ -16,7 +16,8 @@ class PressLinesController < ApplicationController
         flash.now[:notice] += ". " + params[:playlist_time] + ' - ' + params[:playlist_title]
       end
     end  
-    @channel_display = Channel.display
+    remove_v4 = true
+    @channel_display = Channel.display(remove_v4)
 
     respond_to do |format|
       format.html # index.html.erb
@@ -181,7 +182,8 @@ class PressLinesController < ApplicationController
 
   def priority
     @press_lines = PressLine.priority_lines(params[:show], params[:priority_date], params[:channel])     
-    @channel_display = Channel.display
+    remove_v4 = true
+    @channel_display = Channel.display(remove_v4)
     @filter_display = Priority.filter_display
 
     respond_to do |format|
@@ -195,7 +197,8 @@ class PressLinesController < ApplicationController
   def cross_channel_selection
     @cross_channel_priorities = CrossChannelPriority.search(params)
     @press_lines = PressLine.search(params[:search], params[:press_date], params[:channel]) 
-    @channel_display = Channel.display
+    remove_v4 = true
+    @channel_display = Channel.display(remove_v4)
 
     respond_to do |format|
       format.html # cross_channel_selection.html.erb
@@ -203,7 +206,79 @@ class PressLinesController < ApplicationController
     end
   end
   
+  # GET /press_lines/schedule
+  def schedule
+    respond_to do |format|
+      format.html { schedule_for_html }
+      format.xml  { schedule_for_xml }
+    end
+  end
+
+  def schedule_for_html
+    @press_lines = PressLine.schedule_lines(params[:show], params[:priority_date], params[:channel])     
+    remove_v4 = true
+    @channel_display = Channel.display(remove_v4)
+    @filter_display = PressLine.schedule_filter
+    @programmes = PressLine.programme_list(@press_lines)
+    @parts = Part.parts_display
+    params[:programme] = PressLine.selected_programme(@press_lines, params[:programme])
+    logger.debug("============++++++++++++++++=============")
+    logger.debug (params[:programme])
+    @available = AutomatedDynamicSpecial.available_for_schedule(params)
+  end
+
+  def schedule_for_xml
+    @press_lines = PressLine.schedule_lines_for_xml(params[:start_date], params[:channel], params[:days])
+  end
+
+  def add_special
+    specials = PressLineAutomatedDynamicSpecialJoin.find_all_by_press_line_id_and_part_id(params[:id], params[:part_id])
+    if specials.length > 1
+      for index in (specials.length-1).downto(1) do
+        specials[index].destroy
+      end
+    end
+    specials = PressLineAutomatedDynamicSpecialJoin.find_all_by_press_line_id_and_part_id(params[:id], params[:part_id])
+    if specials.length == 1
+      special = specials[0]
+      notice = 'Special schedule updated'
+    else
+      special = PressLineAutomatedDynamicSpecialJoin.new
+      notice = 'Special added to schedule'
+    end
+    special.press_line_id = params[:id]
+    special.automated_dynamic_special_id = params[:ads_id]
+    special.part_id = params[:part_id]
+    special.offset = -240
+
+    if special.save
+      flash[:notice] = notice
+    else
+      flash[:notice] = "Issue with adding/updating special"
+    end
+
+    respond_to do |format|
+      format.html {redirect_to schedule_press_lines_path(:priority_date => params[:priority_date], :channel => params[:channel], :programme => params[:id], :part => params[:part_id], :show => params[:show])}
+      format.xml  { render :xml => @press_lines }
+    end
+  end
   
-  
+  def remove_special
+    special = PressLineAutomatedDynamicSpecialJoin.find(params[:id])
+    if special
+      if special.destroy
+        flash[:notice] = 'Special removed'
+      else
+        flash[:notice] = 'Unable to remove special'
+      end
+    else
+      flash[:notice] = 'Cannot find special'
+    end
+    flash[:notice] = 'Special removed'
+    respond_to do |format|
+      format.html {redirect_to schedule_press_lines_path(:priority_date => params[:priority_date], :channel => params[:channel], :programme => params[:programme], :part => params[:part_id], :show => params[:show])}
+      format.xml  { render :xml => @press_lines }
+    end
+  end
     
 end
