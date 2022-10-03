073a53356c6901035d46c4691f436913e8885a89
David Fuller <david@muvi2.co.uk>
2022-10-03 18:32:11 +0100

    Added search

--- app/models/status.rb
+++ app/models/status.rb
@@ -2,5 +2,13 @@ class Status < ActiveRecord::Base
   
   has_many :media_files
   default_scope :order => :value
-  
+
+  def self.search(params)
+    if params[:message]
+      find_all_by_message(params[:message])
+    else
+      all
+    end
+  end
+
 end
