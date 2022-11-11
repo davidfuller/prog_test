b39afa4daf0cda25368e76ae91f4f4421be5dd20
David Fuller <david@muvi2.co.uk>
2022-11-11 17:47:37 +0000

    add_all_checked_sports_button text

--- app/helpers/comparisons_helper.rb
+++ app/helpers/comparisons_helper.rb
@@ -4,4 +4,8 @@ module ComparisonsHelper
     eidr.starts_with?("com.mtg.content.SPORTS")
   end
 
+  def add_all_checked_sports_button
+    "Add all checked (Create new title. Use for sport)"
+  end
+
 end
