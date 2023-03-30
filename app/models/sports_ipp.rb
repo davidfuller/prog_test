565ab8573269d8d442d43b88b6453cafda72e68c
David Fuller <david@muvi2.co.uk>
2023-03-30 16:36:38 +0100

    Added search and template_name

--- app/models/sports_ipp.rb
+++ app/models/sports_ipp.rb
@@ -4,6 +4,108 @@ class SportsIpp < ActiveRecord::Base
   belongs_to :sports_ipp_media, :dependent => :destroy
   has_one :automated_dynamic_special
 
+  PER_PAGE = 5
+
+  def self.search(params, order)
+    page = params[:page]
+    search = params[:search]
+    template_name = params[:template]
+    if order != :name
+      order = 'created_at DESC'
+    end
+    current_time = Time.current
+    show_all = params[:show_all].present?
+    show_only = params[:show_only].present?
+    
+    if !template_name.present? || template_name == 'All'
+      if search
+        if show_all
+          if show_only
+            paginate  :all, :per_page => PER_PAGE , :page => page,
+                      :conditions => ['sports_ipps.name LIKE ? AND automated_dynamic_specials.last_use <= ? ', "%#{search}%", current_time], :order => order, :joins => [:automated_dynamic_special]
+          else
+            paginate  :all, :per_page => PER_PAGE , :page => page,
+                    :conditions => ['name LIKE ?', "%#{search}%"], :order => order
+          end
+        else # search but not show all
+          if show_only
+            paginate  :all, :per_page => PER_PAGE , :page => page,
+                      :conditions => ['sports_ipps.name LIKE ? AND automated_dynamic_specials.last_use < ?', "%#{search}%", current_time], :order => order, :joins => [:automated_dynamic_special]
+          else
+            paginate  :all, :per_page => PER_PAGE , :page => page,
+                    :conditions => ['sports_ipps.name LIKE ? AND automated_dynamic_specials.last_use > ?', "%#{search}%", current_time], :order => order, :joins => [:automated_dynamic_special]
+          end
+        end
+      else # not search
+        if show_all
+          if show_only
+            paginate  :all, :per_page => PER_PAGE , :page => page,
+                      :conditions => ['automated_dynamic_specials.last_use < ?', current_time], :order => order, :joins => [:automated_dynamic_special]
+          else
+            paginate  :all, :per_page => PER_PAGE , :page => page, :order => order
+          end
+        else # not search and not show all
+          if show_only
+            paginate  :all, :per_page => PER_PAGE , :page => page, 
+                      :conditions => ['automated_dynamic_specials.last_use < ?', current_time], :order => order, :joins => [:automated_dynamic_special]
+          else
+            paginate  :all, :per_page => PER_PAGE , :page => page, 
+                      :conditions => ['automated_dynamic_specials.last_use > ?', current_time], :order => order, :joins => [:automated_dynamic_special]
+          end
+        end
+      end
+    else #specific template
+      template = DynamicSpecialTemplate.find_by_name(template_name)
+      if template
+        if search 
+          if show_all
+            if show_only
+              paginate  :all, :per_page => PER_PAGE , :page => page,
+                        :conditions => ['sports_ipps.name like ? and automated_dynamic_specials.dynamic_special_template_id = ? and automated_dynamic_specials.last_use < ?', "%#{search}%", template.id, current_time], :joins => [:automated_dynamic_special],
+                        :order => order
+            else
+              paginate  :all, :per_page => PER_PAGE , :page => page,
+                        :conditions => ['sports_ipps.name like ? and automated_dynamic_specials.dynamic_special_template_id = ?', "%#{search}%", template.id], :joins => [:automated_dynamic_special],
+                        :order => order
+            end
+          else # template and not show all
+            if show_only
+              paginate  :all, :per_page => PER_PAGE , :page => page,
+                        :conditions => ['sports_ipps.name like ? and automated_dynamic_specials.dynamic_special_template_id = ? AND automated_dynamic_specials.last_use < ?', "%#{search}%", template.id, current_time], :joins => [:automated_dynamic_special],
+                        :order => order
+            else
+              paginate  :all, :per_page => PER_PAGE , :page => page,
+                        :conditions => ['sports_ipps.name like ? and automated_dynamic_specials.dynamic_special_template_id = ? AND automated_dynamic_specials.last_use > ?', "%#{search}%", template.id, current_time], :joins => [:automated_dynamic_special],
+                        :order => order
+            end
+          end
+        else #not search and template
+          if show_all
+            if show_only
+              paginate  :all,:per_page => PER_PAGE , :page => page,
+                        :conditions => ['automated_dynamic_specials.dynamic_special_template_id = ? AND automated_dynamic_specials.last_use < ?', template.id, current_time], :joins => [:automated_dynamic_special],
+                        :order => order
+            else
+              paginate  :all,:per_page => PER_PAGE , :page => page,
+                        :conditions => ['automated_dynamic_specials.dynamic_special_template_id = ?', template.id], :joins => [:automated_dynamic_special],
+                        :order => order
+            end
+          else #not search and template and not show all
+            if show_only
+              paginate  :all,:per_page => PER_PAGE , :page => page,
+                        :conditions => ['automated_dynamic_specials.dynamic_special_template_id = ? AND automated_dynamic_specials.last_use < ?', template.id, current_time], :joins => [:automated_dynamic_special],
+                        :order => order
+            else
+              paginate  :all,:per_page => PER_PAGE , :page => page,
+                        :conditions => ['automated_dynamic_specials.dynamic_special_template_id = ? AND automated_dynamic_specials.last_use > ?', template.id, current_time], :joins => [:automated_dynamic_special],
+                        :order => order
+            end
+          end
+        end
+      end
+    end
+  end
+
   def self.new_name(ads_name)
     if find_by_name(ads_name)
       version = 1
@@ -94,6 +196,7 @@ class SportsIpp < ActiveRecord::Base
 
     media_file.filename = zip_filename
     media_file.sports_ipp_status = SportsIppStatus.find_by_message('Not Loaded')
+    media_file.media_folder_id = MediaFolder.find_by_name('Sports IPP Package').id
     media_file.save
     sports_ipp.sports_ipp_media = media_file
     sports_ipp.save
@@ -101,6 +204,14 @@ class SportsIpp < ActiveRecord::Base
 
   def create_filename(name, extension)
     Useful.strip_filename(name).upcase + extension
+  end
+
+  def template_name
+    if automated_dynamic_special.present? && automated_dynamic_special.dynamic_special_template.present?
+      automated_dynamic_special.dynamic_special_template.name
+    else
+      'No template'
+    end
   end
 
 end
